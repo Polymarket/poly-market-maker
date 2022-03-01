7590d8c8de50b8074d944a2c621f253f39595af2
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-03-01 13:31:54 -0500

    fix: rm empty tests

--- tests/test_orderbook.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class TestOrderbook(unittest.TestCase):
-
-    def test_pass(self):
-        pass
-    pass
--- tests/test_utils.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-
-class TestUtils(TestCase):
-
-    def test_pass(self):
-        pass
-    
