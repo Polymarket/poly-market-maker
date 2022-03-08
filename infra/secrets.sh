c5972af1700425e9a705daffef14700bc3142c37
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2022-03-08 16:31:33 -0500

    chore:fix secrets.sh

--- infra/secrets.sh
+++ infra/secrets.sh
@@ -1,5 +1,5 @@
 # Create secrets for market-maker-keeper staging
-kubectl ~/.kube/kube.config create secret generic mmk-staging-secrets \
+kubectl --kubeconfig ~/.kube/kube.config create secret generic mmk-staging-secrets \
   --from-literal=rpc_url="https://polygon-mumbai.infura.io/v3/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
   --from-literal=pk="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" \
   --from-literal=clob_api_key="xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxxx" \
