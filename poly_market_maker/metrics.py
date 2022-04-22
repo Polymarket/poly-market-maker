c309afed06b1c256f45439f0699788578c3f9c84
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2022-04-22 17:39:36 -0300

    fixing metrics

--- poly_market_maker/metrics.py
+++ poly_market_maker/metrics.py
@@ -1,26 +1,26 @@
 from prometheus_client import Counter, Gauge, Histogram
 
 chain_requests_counter = Counter(
-    "requests_counter",
+    "chain_requests_counter",
     "Counts the chain executions",
     labelnames=["method", "status"],
-    namespace="arbitrage_keeper_chain",
+    namespace="market_maker",
 )
 keeper_balance_amount = Gauge(
     "balance_amount",
     "Balance of the bot",
     labelnames=["accountaddress", "assetaddress", "tokenid"],
-    namespace="arbitrage_keeper_chain",
+    namespace="market_maker",
 )
 clob_requests_latency = Histogram(
     "clob_requests_latency",
     "Latency of the clob requests",
     labelnames=["method", "status"],
-    namespace="arbitrage_keeper_chain",
+    namespace="market_maker",
 )
 gas_station_latency = Histogram(
     "gas_station_latency",
     "Latency of the gas station",
     labelnames=["strategy", "status"],
-    namespace="arbitrage_keeper_chain",
+    namespace="market_maker",
 )
